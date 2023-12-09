20a216827bf749777f7cb3b60b61b9f0a2172554
luannfirme <luannfirme@gmail.com>
2023-12-09 16:18:43 -0300

    Finalização de atividade

--- Semana 5/FEB-P005/ts/script.ts
+++ Semana 5/FEB-P005/ts/script.ts
@@ -2,8 +2,8 @@ async function ObterTemperaturaAtual() {
     try {
         const resposta = await fetch('https://api.openweathermap.org/data/3.0/onecall?lat=-14.796581726513459&lon=-39.173008679879025&exclude=minutely,hourly&units=metric&appid=70fd9d2c18a3ccd0fadf64377139e91d&lang=pt_br');
         const temperatura = await resposta.json();
-        const temperaturaAtual = temperatura.current.temp + 'ºC';
-        const descricaoTempoAtual = temperatura.current.weather[0].description;
+        const temperaturaAtual = Math.round(temperatura.current.temp) + 'ºC';
+        const descricaoTempoAtual = temperatura.current.weather[0].description.charAt(0).toUpperCase() + temperatura.current.weather[0].description.slice(1);
         const tempMaximaHoje = Math.round(temperatura.daily[0].temp.max) + 'ºC';
         const tempMinimaHoje = Math.round(temperatura.daily[0].temp.min) + 'ºC';
         return { temperaturaAtual, descricaoTempoAtual, tempMaximaHoje, tempMinimaHoje };
@@ -17,9 +17,13 @@ async function obterNoticias() {
     try {
         const resposta = await fetch('https://newsapi.org/v2/everything?sortBy=publishedAt&apiKey=e10349f1a5334964a73e16a7740b669a&q=Universidade Estadual de Santa Cruz&language=pt');
         const noticias = await resposta.json();
-        const noticia = noticias.map((noticia: any) => noticia.latlng);
-        console.log("Coordenada: " + coordenada[0]);
-        return coordenada;
+        const titulosELinks = noticias.articles.map((noticia: any) => {
+            return {
+                titulo: noticia.title,
+                link: noticia.url
+            };
+        });
+        return titulosELinks;
     }
     catch (error) {
         console.error('Erro com a chamada fetch: ', error);
@@ -30,10 +34,21 @@ const divNoticias = document.getElementById('listaNoticias');
 const divServicos = document.getElementById('listaServicos');
 
 ObterTemperaturaAtual().then(temp => {
-    const retornoTemperatura = `Temperatura atual: ${temp?.temperaturaAtual}<br>
-    ${temp?.descricaoTempoAtual}<br>
-    Máx.: ${temp?.tempMaximaHoje} &nbsp;&nbsp; Min.: ${temp?.tempMinimaHoje}<br>`;
+    const retornoTemperatura = `Temperatura atual:<br>
+    <b><font size='15'>${temp?.temperaturaAtual}</font></b><br>
+    <b>${temp?.descricaoTempoAtual}</b><br>
+    Máx.: <font color='red'>${temp?.tempMaximaHoje}</font> &nbsp;&nbsp; Min.:<font color='blue'> ${temp?.tempMinimaHoje}</font><br>`;
     if (divServicos) {
         divServicos.innerHTML = retornoTemperatura;
     }
 });
+
+obterNoticias().then(noticias => noticias.forEach((noticia:any) => {
+    const novaNoticia = document.createElement("a");
+    novaNoticia.innerHTML = `<b>${noticia.titulo}</b><br>`;
+    novaNoticia.href = `${noticia.link}`;
+    novaNoticia.target= '_blank';
+    if (divNoticias) {
+        divNoticias.appendChild(novaNoticia);
+    }
+}));
